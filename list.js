760a6714873009865523d5df784919939890866f
남우현 <skyainq@naver.com>
2023-07-18 11:17:02 +0900

    to laptop

--- list.js
+++ list.js
@@ -4,7 +4,52 @@ var products = [
     { id: 2, price: 60000, title: 'Black Monastery' }
 ];
 
-for(let i = 0; i < 3; i ++){
-    document.querySelectorAll('.car-name')[i].innerHTML = products[i].title;
-    document.querySelectorAll('.car-price')[i].innerHTML = `가격: ${products[i].price}`;
+function getProducts(array) {
+    array.forEach((a, i) => {
+        var card = `<div class="col-sm-4">
+        <img src="https://via.placeholder.com/600" class="w-100">
+        <h5>${a.title}</h5>
+        <p>가격 : ${a.price}</p>
+    </div>`;
+
+        document.querySelector('.row').insertAdjacentHTML("beforeend", card)
+    });
 }
+
+getProducts(products);
+
+var more = 1;
+
+document.querySelector('#more').addEventListener('click', function () {
+    fetch(`https://codingapple1.github.io/js/more${more}.json`)
+        .then(res => res.json())
+        .then(function (data) {
+            getProducts(data);
+            more++;
+            if (more > 2) {
+                document.getElementById('more').style.display = 'none';
+            }
+        })
+})
+
+
+
+
+// for (let i = 0; i < products.length; i++) {
+//     var a = `<div class="col-sm-4">
+//                 <img src="https://via.placeholder.com/600" class="w-100">
+//                 <h5>${products[i].title}</h5>
+//                 <p>가격 : ${products[i].price}</p>
+//             </div>`;
+//     document.querySelector('.row').insertAdjacentHTML("beforeend", a);
+// }
+
+// document.querySelector('#more').addEventListener('click', function () {
+//     fetch('https://codingapple1.github.io/js/more1.json')
+//         .then(res => res.json())
+//         .then(function (data) {
+//             console.log(data);
+//         }).catch(function (error) {
+//             console.log('오류남');
+//         });
+// });
